75173f19372ab589b566a9d2fc8e89a3d6a3900c
kphimagrind <kphimagrind@gmail.com>
2026-06-09 18:29:28 +0700

    hapus file

--- arcvhive-new/app/core/router.js
+++
@@ -1,8 +0,0 @@
-window.Router = {
-  current: function() {
-    return document.body.dataset.page || 'home';
-  },
-  navigate: function(target) {
-    window.location.href = AppUtils.resolveLink(target);
-  }
-};
